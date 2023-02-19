a4c08514408fff3c87ea89b63b9e4f0e26706777
hybo <52079867+me1ting@users.noreply.github.com>
2023-02-19 12:56:35 +0800

    Use new format for stats

--- scripts/format.py
+++ scripts/format.py
@@ -8,8 +8,23 @@ def load_json(file):
     return data
 
 
+def format(template: str):
+    arg_index = 0
+    while "(\\S+)" in template:
+        template = template.replace("(\\S+)", f"{{{arg_index}}}", 1)
+        arg_index += 1
+
+    if template[0] == "^":
+        template = template[1:]
+
+    if template[-1] == "$":
+        template = template[:-1]
+
+    return template
+
+
 if __name__ == "__main__":
-    files = ["../src/stats.json"]
+    files = ["../src/stats/main.json", "../src/stats/append.json"]
 
     for file in files:
         with open(file, 'rt', encoding="utf-8") as f:
@@ -17,20 +32,9 @@ if __name__ == "__main__":
 
             statlist = []
 
-            for stat, stat_content in data.items():
-                if "text" in stat_content:
-                    texts = stat_content["text"]
-
-                    if "0" in texts:
-                        texts_0 = texts["0"]
-                    if "1" in texts:
-                        texts_1 = texts["1"]
-
-                    #print(texts_1)
-
-                    for id in texts_1:
-                        if id in texts_0:
-                            statlist.append({"id": stat, "zh": texts_1[id], "en": texts_0[id]})
+            for stat in data:
+                stat["zh"] = format(stat["zh"])
+                stat["en"] = format(stat["en"])
 
             with open(f'{file}.new.json', 'wt', encoding="utf-8") as f:
-                f.write(json.dumps(statlist, ensure_ascii=False))
+                f.write(json.dumps(data, ensure_ascii=False, indent=4))
