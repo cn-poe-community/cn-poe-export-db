7ccc4f2c6f5ce97cace4c6da57c5eb448eb1e761
hybo <52079867+me1ting@users.noreply.github.com>
2023-04-23 19:49:42 +0800

    Generate keystone stats when compile

--- compile_to_ts.py
+++ compile_to_ts.py
@@ -29,6 +29,7 @@ types["ascendant.json"] = "Node[]"
 types["keystones.json"] = "Node[]"
 types["notables.json"] = "Node[]"
 
+
 def emptyDir(dir):
     try:
         shutil.rmtree(dist)
@@ -36,26 +37,60 @@ def emptyDir(dir):
         print("dist is cleaned")
     os.mkdir(dist, 0o666)
 
+
 def load_json(file):
     with open(file, 'rt', encoding='utf-8') as f:
         content = f.read()
         data = json.loads(content)
     return data
 
-def json2code(json,variableName, type):
+
+def json2code(json, variableName, type):
     if variableName == "requirement_suffixes":
         variableName = "requirementSuffixes"
     return f"export const {variableName}: {type} = {json};"
 
+
 def make_stats():
     stats_folder = os.path.join(src, "stats")
     all_data = []
+    # add all stats/*.json
     for file_name in os.listdir(stats_folder):
         if file_name.endswith(".json"):
             data: list = load_json(os.path.join(stats_folder, file_name))
             all_data.extend(data)
-    
+    # add jewel-mods
+    keystones = load_json(os.path.join(src, "passiveskills", "keystones.json"))
+    for keystone in keystones:
+        zh = keystone["zh"]
+        en = keystone["en"]
+        all_data.append({"zh":f"{zh}范围内的天赋可以在\n未连结至天赋树的情况下配置","en":f"Passives in Radius of {en} can be Allocated\nwithout being connected to your tree"})
+
+    all_data = removeId(all_data)
+    all_data = removeRepeats(all_data)
+
     return all_data
+
+def removeId(stats):
+    return [{"zh": item["zh"], "en": item["en"]} for item in stats]
+
+def removeRepeats(stats):
+    stat_list =[]
+    stat_map = {}
+    for stat in stats:
+        zh = stat["zh"]
+        en = stat["en"]
+        if zh in stat_map:
+            old_en = stat_map[zh]["en"]
+            if en!=old_en:
+                print("warning: same zh but diff en")
+                print(f"{zh}")
+                print(f"{old_en}")#old
+                print(f"{en}")#old
+                continue
+        stat_list.append(stat)
+        stat_map[zh] = stat
+    return stat_list
 
 def make():
     content = [imports]
@@ -76,11 +111,12 @@ def make():
             content.append(code)
 
     stats = make_stats()
-    stats_code = json2code(stats, "stats" , types["stats.json"])
+    stats_code = json2code(stats, "stats", types["stats.json"])
 
     content.append(stats_code)
-    with open(os.path.join(dist, "assets.ts"), 'wt', encoding="utf-8") as f:
+    with open(os.path.join(dist, "assets.ts"), 'wt', encoding="utf-8", newline="\n") as f:
         f.write("\n".join(content))
+
 
 emptyDir(dist)
 make()
